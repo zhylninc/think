b9ff8527facbb8fccc9da030c9bbb9b97934de3f
7IN0SAN9 <me@7in0.me>
2016-01-12 15:29:40 +0800

    在 before_script 增加 php 版本检测

---
+++ tests/script/before_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $(phpenv version-name) != "hhvm" ]; then
+
+    pecl channel-update pecl.php.net
+
+    if [ $(phpenv version-name) = "7.0" ]; then
+        echo "yes" | pecl install apcu-5.1.2
+    else
+        echo "yes" | pecl install apcu-4.0.10
+    fi
+fi
+
+phpenv config-add tests/myconfig.ini
+
+composer self-update && composer install
